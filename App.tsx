7acd250b742dfb481cb50e178bbfbf172c3a52ad
a1a2a3a4a5 <linub7@gmail.com>
2023-07-03 11:09:04 +0430

    start project & make initial configurations -> done

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react';
+import {StyleSheet, View} from 'react-native';
+
+interface Props {}
+
+const App: FC<Props> = () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default App;
---
+++ babel.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@utils': './src/utils',
+          '@views': './src/views',
+          '@ui': './src/ui',
+          src: './src',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        blacklist: null, // DEPRECATED
+        whitelist: null, // DEPRECATED
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
+};
